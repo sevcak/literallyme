3c9c2d99d9ccedf73af1b9c6128748ed353f088c
sevcak <sevclukas0@gmail.com>
2023-07-16 14:52:54 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+      <p>this month, you're literally:</p>
+    </div>
+  );
+}
+
+export default App;
