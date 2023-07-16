8e775a6c8ce99dcf594ede443f967f621fd0fef5
sevcak <sevclukas0@gmail.com>
2023-07-16 19:08:10 +0200

    fix: fixed too large spacings and horizontal scroll

--- src/App.tsx
+++ src/App.tsx
@@ -54,17 +54,17 @@ const App = () => {
   return (
     <div
       className='
-        w-screen min-h-screen p-6 pt-10 md:pt-16 lg:pt-20 xl:pt-24
+        w-screen min-h-screen p-6 pt-10 2xl:pt-16 3xl:pt-20 4xl:pt-24
         flex flex-col items-center justify-between text-center text-2xl
-        bg-gradient-to-t from-stone-200 to-40%
+        bg-gradient-to-t from-stone-200 to-40% overflow-hidden
       '
     >
-      <div className='space-y-4 md:space-y-8'>
+      <div className='space-y-4 2xl:space-y-8'>
         <p>this month, you're literally:</p>
         {
           personality &&
-          <div className={`flex flex-col gap-6 items-center ${isRolling ? 'scale-90' : ''} duration-200`}>
-            <h1 className='max-sm:text-3xl max-sm:font-bold max-sm:-mx-4'>
+          <div className={`flex flex-col gap-3 2xl:gap-6 items-center ${isRolling ? 'scale-90' : ''} duration-200`}>
+            <h1 className='max-sm:text-3xl max-sm:font-bold max-sm:-mx-4 text-4xl 2xl:text-5xl'>
               {
                 isRolling
                 ? <p>{personality.name}</p> 
@@ -76,22 +76,22 @@ const App = () => {
               ? (
                 <img
                   src={personality.img} alt={personality.name}
-                  className='w-64 h-80 object-cover object-top rounded-lg'
+                  className='w-64 h-72 2xl:h-80 object-cover object-top rounded-lg'
                 />
               ) : (
                 <a href={personality.img} className='hover:brightness-150 hover:contrast-50'>
                 <img
                   src={personality.img} alt={personality.name}
-                  className='w-64 h-80 object-cover object-top rounded-lg'
+                  className='w-64 h-72 2xl:h-80 object-cover object-top rounded-lg'
                 />
                 </a>
               )
             }
             {
               (!isRolling && personality.from) &&
-              <div className='space-y-2'>
+              <div className='space-y-1 2xl:space-y-2'>
                 <p>from</p>
-                <h2 className='max-sm:text-2xl max-sm:font-bold'>
+                <h2 className='max-sm:text-2xl max-sm:font-bold text-3xl 2xl:text-4xl'>
                   <a href={personality.from.link}>{personality.from.title}</a>
                 </h2>
               </div>
@@ -101,11 +101,11 @@ const App = () => {
         {
           (!isRolling && personality) &&
           // Buttons
-          <div className='md:mt-4 flex flex-col items-center gap-4 md:gap-8'>
+          <div className='2xl:mt-4 flex flex-col items-center gap-4 2xl:gap-8'>
             {/* Tweet button */}
             <a
               className='
-                bg-gray-300 text-sm p-2 md:p-4 w-fit h-fit rounded-full
+                bg-gray-300 text-sm p-2 2xl:p-4 w-fit h-fit rounded-full
                 border border-gray-300 hover:border-indigo-500 duration-75
               '
               href={
@@ -118,7 +118,7 @@ const App = () => {
                 + '%0Asevcak.github.io/literallyme by @_sevcak'
               }
             >
-              <img className='w-4 h-4 md:w-8 md:h-8' src="./assets/twitter.svg" alt="Tweet" />
+              <img className='w-4 h-4 2xl:w-8 2xl:h-8' src="./assets/twitter.svg" alt="Tweet" />
             </a>
             <button
               className='bg-gray-300 text-sm p-1 px-2'
