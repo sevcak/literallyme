1ecea06825bc0f2b22eeee6153206fd285a256e0
sevcak <sevclukas0@gmail.com>
2023-07-16 15:39:48 +0200

    feat: add personalities data file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
       <p>this month, you're literally:</p>
+      
     </div>
   );
 }
